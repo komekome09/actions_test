8f96973b05bcece0ab350cc32270446222d4d53d
komekome09 <komekome09@users.noreply.github.com>
2020-12-21 15:16:54 +0000

    deploy: 07e3b95d77e164992ffd04321fbe8076b9fa5363

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
-var canvasElem = document.getElementsByTagName('canvas');
+let canvasElem = document.getElementsByTagName('canvas');
 if (canvasElem === null)
     throw new Error('Cannot get element "canvas"');
-var context = canvasElem[0].getContext('2d');
+let context = canvasElem[0].getContext('2d');
 if (context === null)
     throw new Error('Cannot get context');
 const range = (begin, end) => ([...Array(end - begin)].map((_, i) => (begin + i)));
 function row_lines() {
     if (context === null)
         return;
-    for (var i of range(1, context.canvas.height / 50 + 1)) {
+    for (let i of range(1, context.canvas.height / 50 + 1)) {
         context.save();
         context.beginPath();
         context.lineWidth = 1;
@@ -20,6 +20,17 @@ function row_lines() {
         context.restore();
     }
 }
+let columnIndex = {
+    on_time: 10,
+    change_time: 100,
+    destination_jp: 190,
+    destination_en: 370,
+    flight_number: 500,
+    terminal: 620,
+    gate: 780,
+    flight_status: 880
+};
+let flightsData = [];
 function draw_text() {
     if (context === null)
         return;
@@ -27,15 +38,21 @@ function draw_text() {
     context.textAlign = "left";
     context.textBaseline = "middle";
     context.fillStyle = "rgb(200, 200, 200)";
-    var columnData = [{ x: 10, name: "定刻" }, { x: 100, name: "変更" }, { x: 190, name: "行先" },
+    let columnData = [{ x: 10, name: "定刻" }, { x: 100, name: "変更" }, { x: 190, name: "行先" },
         { x: 370, name: "" }, { x: 500, name: "便名" }, { x: 620, name: "ターミナル" }, { x: 780, name: "搭乗口" },
         { x: 880, name: "運行状況" }];
-    var testData = ["21:30", "22:00", "札幌(新千歳)", "Sapporo", "NH3848", "1", "17", "搭乗手続中"];
     context.fillText("出発 Departures", 10, 30);
-    for (const [key, value] of columnData.entries()) {
+    for (const value of columnData) {
         context.fillText(value.name, value.x, 30 + 50);
-        context.fillText(testData[key], value.x, 30 + 50 * 2);
     }
+    for (const [key, value] of flightsData.slice(-14).entries()) {
+        console.log(key + "|" + value, columnIndex.on_time);
+    }
+}
+function add_flightData(flightData) {
+    if (flightData === null)
+        return;
+    flightsData.push(flightData);
 }
 function background() {
     if (context === null)
@@ -50,27 +67,40 @@ function draw() {
 }
 setInterval(draw, 50);
 function get_data() {
-    console.log("hi");
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const proxy = 'https://blooming-lowlands-21185.herokuapp.com/';
     const url = proxy + 'https://tokyo-haneda.com/app_resource/flight/data/dms/hdacfdep.json';
     const AxiosInstance = axios.create();
     AxiosInstance.get(url)
         .then(response => {
         const json = response.data;
         console.log(json);
-        for (var i of json.flight_info) {
-            var timeEst = new Date(i.定刻).getTime();
-            var timeChn = new Date(i.変更時刻).getTime();
-            var timeRea = 0;
+        if (json.flight_end === true) {
+            console.log("Today's flight is ended");
+            return;
+        }
+        for (let i of json.flight_info) {
+            let timeEst = new Date(i.定刻).getTime();
+            let timeChn = new Date(i.変更時刻).getTime();
+            let timeRea = 0;
             if (timeChn === NaN)
                 timeRea = timeEst;
             else
                 timeRea = timeEst;
-            var nowTime = new Date().getTime();
-            if (timeRea < nowTime)
+            let nowTime = new Date().getTime();
+            if (timeRea < nowTime && false)
                 continue;
-            var flightData = [i.定刻, i.変更時刻, i.行先地空港和名称, i.行先地空港英名称, i.航空会社[0].ＡＬコード + i.航空会社[0].便名, i.ターミナル区分, i.ゲート和名称, i.備考和名称];
+            let flightData = {
+                on_time: i.定刻,
+                change_time: i.変更時刻,
+                destination_jp: i.行先地空港和名称,
+                destination_en: i.行先地空港英名称,
+                flight_number: i.航空会社[0].ＡＬコード + i.航空会社[0].便名,
+                terminal: i.ターミナル区分,
+                gate: i.ゲート和名称,
+                flight_status: i.備考和名称
+            };
             console.log(flightData);
+            add_flightData(flightData);
         }
     })
         .catch(console.error);
